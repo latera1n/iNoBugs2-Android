e555f4e364a1fbe46c9edaeac6dcc23e1783fbc0
Yuchi Deng <dengyuchi@gmail.com>
2017-02-27 17:15:22 -0800

    02/27 Add bug result layout

---
+++ app/src/main/java/io/laterain/inobugs2/activity/ResultActivity.java
@@ -0,0 +1,15 @@
+package io.laterain.inobugs2.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.laterain.inobugs2.R;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
